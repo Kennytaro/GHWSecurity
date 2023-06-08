6358dd77c2bc02f46c7576d2f33ebca987ca788c
Kennytaro <119624844+Kennytaro@users.noreply.github.com>
2023-06-08 04:59:36 +0000

    Append and write files

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-with open("apache_acess.bib", "r") as file:
-    for line in file:
-        elements = line.split(" ")
-        transaction = {
-            'ip' : elements[0],
-            'timestamp' : "".join([elements[3].strip("[]"),elements[4].strip("[]")]),
-            'kind_of_petition' : elements[5],
-            'route' : elements[6],
-            'version' : elements[7].strip('"'),
-            'status' : elements[8],
-            'completed_time' : elements[9].strip('/n')
-        }
+#with open("apache_acess.bib", "r") as file:
+#   for line in file:
+#       elements = line.split(" ")
+#       transaction = {
+#           'ip' : elements[0],
+#           'timestamp' : "".join([elements[3].strip("[]"),elements[4].strip("[]")]),
+#           'kind_of_petition' : elements[5],
+#          'route' : elements[6],
+#          'version' : elements[7].strip('"'),
+#          'status' : elements[8],
+#           'completed_time' : elements[9].strip('/n')
+#     }
         
 
-        print(transaction)
+with open("number.txt","a") as file:
+    for number in range(16,21):
+        file.write(str(number) + "\n")
